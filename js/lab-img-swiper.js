08f81995ee772ddcf8a25af1021e3c2a4da7d0f4
Paulo Turco <pauloturcko@gmail.com>
2024-04-30 13:50:37 -0300

    feature

--- js/lab-img-swiper.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    new Splide('#vertical-slider', {
-        type: 'slide',
-        direction: 'ttb', // top to bottom
-        height: '300px', // altura do slide
-    }).mount();
-});
--- js/scroll-reveal-credenciamentos.js
+++
@@ -1,7 +0,0 @@
-window.sr = ScrollReveal({ reset: true});
-
-sr.reveal ('', {
-    distance: '-90px',
-    duration: 2000,
-    delay: 600
-});
