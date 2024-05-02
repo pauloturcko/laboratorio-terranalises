accd7e50d6ab75555142cd7b773a67588f55314b
Paulo Turco <pauloturcko@gmail.com>
2024-05-02 17:15:03 -0300

    responsive finished

--- js/items-div-ambiental.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    var items = document.querySelectorAll('.sub-servicos-bt');
-    var delay = 100;
-
-    for (var i = 0; i < items.length; i++) {
-        items[i].style.transitionDelay = delay * i + 'ms';
-        items[i].style.opacity = '1';
-    }
-}
