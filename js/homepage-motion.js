854765e30ca16033c04021ae16c2367d159b86c7
Paulo Turco <pauloturcko@gmail.com>
2024-05-03 19:04:55 -0300

    motion

---
+++ js/homepage-motion.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({reset: false})
+
+sr.reveal('.cabecalho-area', {
+    duration: 1500,
+    distance: '-4em',
+});
+
+sr.reveal('.cabecalho-area', {
+    duration: 1500,
+    distance: '-4em',
+});
+
+sr.reveal('.cards-container', {
+    duration: 1500,
+    distance: '4em',
+});
+
+sr.reveal('.sobre-texto', {
+    duration: 1500,
+    distance: '4em',
+    delay: 500,
+});
+
+sr.reveal('.slider', {
+    duration: 1500,
+    distance: '4em',
+    delay: 900,
+});
--- notes.md
+++
@@ -1,3 +0,0 @@
-Timeline: https://codepen.io/ritz078/pen/LGRWjE
-
-MegaDropDown: https://codyhouse.co/gem/mega-dropdown/
