f6dc0c6b4f29d6856d200494d4293be618e155d6
Paulo Turco <pauloturcko@gmail.com>
2024-05-07 18:07:20 -0300

    motion

--- js/div-agr-subservicos-motion.js
+++ js/div-agr-subservicos-motion.js
@@ -5,35 +5,21 @@ sr.reveal('.cabecalho-area', {
     distance: '-4em',
 });
 
-sr.reveal('.cardOne', {
-    origin: 'right',
-    distance: '100%',
-    duration: 1900,
-    easing: 'cubic-bezier(.5,0,0,1)'
-});
-
-sr.reveal('.cardThree', {
-    origin: 'right',
-    distance: '100%',
-    duration: 1900,
-    delay: 400,
-    easing: 'cubic-bezier(.5,0,0,1)'
-});
-
-sr.reveal('.cardTwo', {
+sr.reveal('.motionCardOne', {
     origin: 'left',
     distance: '100%',
     duration: 1900,
-    delay: 200,
     easing: 'cubic-bezier(.5,0,0,1)'
 });
 
-sr.reveal('.texto', {
+sr.reveal('.motionCardTwo', {
+    origin: 'right',
+    distance: '100%',
+    duration: 1900,
+    easing: 'cubic-bezier(.5,0,0,1)'
+});
+
+sr.reveal('.beneficio-card', {
     duration: 1500,
     distance: '4em',
 });
-
-sr.reveal('.mvv', {
-    duration: 2000,
-    distance: '4em',
-})
